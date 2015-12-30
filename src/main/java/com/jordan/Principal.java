4fb19eaaa3c42ec52451f531ca47916bf9094cec
jordangrego <jordangrego@gmail.com>
2015-12-30 16:08:02 -0200

    commit1234

--- src/main/java/com/jordan/Principal.java
+++ src/main/java/com/jordan/Principal.java
@@ -3,5 +3,6 @@ package com.jordan;
 public class Principal {
 	public void teste() {
 		System.out.println("teste");
+		System.out.println("alow");
 	}
 }
---
+++ src/main/java/com/jordan/TesteJordan.java
@@ -0,0 +1,7 @@
+package com.jordan;
+
+public class TesteJordan {
+	public void executarRotina() {
+		System.out.println("teste");
+	}
+}
